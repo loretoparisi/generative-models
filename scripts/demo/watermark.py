9dae196e031b1c48f02d946521275e1704fe1928
EC2 Default User <ec2-user@ip-10-10-13-220.ec2.internal>
2023-11-23 12:47:50 +0000

    Updated watermark example

--- scripts/demo/watermark.py
+++ scripts/demo/watermark.py
@@ -1,11 +1,17 @@
+import os, sys
+BASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+ASSETS = BASE_PATH.split(os.sep)[:-2]
+ASSETS=f'{os.sep}'.join(ASSETS)
+ASSETS=os.path.join(ASSETS,'assets')
+
 import cv2
 from imwatermark import WatermarkEncoder
 
-bgr = cv2.imread('../../assets/original.png')
+bgr = cv2.imread(os.path.join(ASSETS,'original.jpg'))
 wm = 'test'
 
 encoder = WatermarkEncoder()
 encoder.set_watermark('bytes', wm.encode('utf-8'))
 bgr_encoded = encoder.encode(bgr, 'dwtDct')
 
-cv2.imwrite('../../assets/test_wm.png', bgr_encoded)
+cv2.imwrite(os.path.join(ASSETS,'original_wm.png'), bgr_encoded)
